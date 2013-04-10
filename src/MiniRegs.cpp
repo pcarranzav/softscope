08b60b777101db067fd13cf8c0b176c7a11a0fa2
Pablo Carranza Vélez <pablocarranza@gmail.com>
2013-04-10 20:29:07 -0300

    Corrected some includes in MiniRegs

--- src/MiniRegs.cpp
+++ src/MiniRegs.cpp
@@ -1,23 +1,24 @@
 #include <MiniRegs.hpp>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 using namespace std;
 
 MiniRegs::MiniRegs(string driver, int length, int base)
 {
-	int fd = open(driver.data(), O_RDWR);
-	regsMap = (int*) mmap(0, length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, base);
-	close(fd);
+	//int fd = open(driver.data(), O_RDWR);
+	//regsMap = (int*) mmap(0, length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, base);
+	//close(fd);
 }
 
 int MiniRegs::read(int address)
 {
-	return regsMap[address >> 2];
+	//return regsMap[address >> 2];
 }
 
 void MiniRegs::write(int address, int value)
 {
-	regsMap[address >> 2] = value;
+	//regsMap[address >> 2] = value;
 }
 
